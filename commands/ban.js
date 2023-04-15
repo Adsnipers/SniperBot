7f8cdd9e8eb2365661812855c744a903a97d3eba
Adsnipers <38774365+Adsnipers@users.noreply.github.com>
2023-04-15 08:43:59 +0000

    Ban command for SniperBot Administrators

--- commands/ban.js
+++ commands/ban.js
@@ -1,41 +1,47 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
+	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ban')
 		.setDescription('Bans a user')
 		.addUserOption(option =>
-		option
-			.setName('target')
-			.setDescription('The user to ban')
-			.setRequired(true))
-			.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-			.setDMPermission(false),
-		
+			option
+				.setName('target')
+				.setDescription('The user to ban')
+				.setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+		.setDMPermission(false),
+
 	async execute(interaction) {
 		const { MongoClient } = require("mongodb");
 		const uri = process.env.MONGODB_URI;
 		const dbClient = new MongoClient(uri);
+		const guild = interaction.guild
 		async function run() {
 			try {
-			  const database = dbClient.db('sb_users');
-			  const admins = database.collection('admins');
-			  const query = { uid: `${interaction.user.id}` };
-			  const user = await admins.findOne(query);
-				
-			// Respond to the command with user information
-			// If user exists in the database, print database information, if not, print base discord information.
-			if (user != null && user.permission_level >= 1) {
-			  const user = interaction.options.getUser('target');
-				guild.members.ban(user);
-			} else {
-				
-			};
+				const database = dbClient.db('sb_users');
+				const admins = database.collection('admins');
+				const query = { uid: `${interaction.user.id}` };
+				const user = await admins.findOne(query);
+
+				// Check if command initiator is a global admin
+				// ban mentioned user
+				// If not global admin, check if Server Admin
+				// Ban mentioned user
+				if (user != null) {
+					const banuser = interaction.options.getUser('target');
+					await guild.members.ban(banuser);
+					await interaction.reply(`Banned ${banuser}`);
+					console.log(`${user.nickname} banned ${banuser} from ${guild.name}`)
+				} else {
+					await interaction.reply(`Bro, Just right click them....`);
+				};
 			} finally {
-			  // Ensure the MongoDB client is closed
-			  await dbClient.close();
+				// Ensure the MongoDB client is closed
+				await dbClient.close();
 			}
-		  }
-		  run().catch(console.dir);
+		}
+		run().catch(console.dir);
 	},
 };
--- commands/user.js
+++ commands/user.js
@@ -4,30 +4,30 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Provides information about the user.'),
-		
+
 	async execute(interaction) {
 		const { MongoClient } = require("mongodb");
 		const uri = process.env.MONGODB_URI;
 		const dbClient = new MongoClient(uri);
 		async function run() {
 			try {
-			  const database = dbClient.db('sb_users');
-			  const admins = database.collection('admins');
-			  const query = { uid: `${interaction.user.id}` };
-			  const user = await admins.findOne(query);
-				
-			// Respond to the command with user information
-			// If user exists in the database, print database information, if not, print base discord information.
-			if (user != null) {
-			  await interaction.reply(`Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rYou joined this server on ${interaction.member.joinedAt} \rand your role in my backend system is ***${user.role}***`);
-			} else {
-				await interaction.reply(`Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rand you joined this server on ${interaction.member.joinedAt}`);
-			};
+				const database = dbClient.db('sb_users');
+				const admins = database.collection('admins');
+				const query = { uid: `${interaction.user.id}` };
+				const user = await admins.findOne(query);
+
+				// Respond to the command with user information
+				// If user exists in the database, print database information, if not, print base discord information.
+				if (user != null) {
+					await interaction.reply(`Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rYou joined this server on ${interaction.member.joinedAt} \rand your role in my backend system is ***${user.role}***`);
+				} else {
+					await interaction.reply(`Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rand you joined this server on ${interaction.member.joinedAt}`);
+				};
 			} finally {
-			  // Ensure the MongoDB client is closed
-			  await dbClient.close();
+				// Ensure the MongoDB client is closed
+				await dbClient.close();
 			}
-		  }
-		  run().catch(console.dir);
+		}
+		run().catch(console.dir);
 	},
 };
