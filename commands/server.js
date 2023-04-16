d90d585d02a6beca1aca069a965284b048a4755d
Adsnipers <38774365+Adsnipers@users.noreply.github.com>
2023-04-16 07:32:33 +0000

    Code cleanup

--- commands/server.js
+++ commands/server.js
@@ -1,29 +1,33 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('server')
-		.setDescription('Provides information about the server.'),
-	async execute(interaction) {
-		// launch MongoDB connection and query database
-		const { MongoClient } = require("mongodb");
-		const uri = process.env.MONGODB_URI;
-		const dbClient = new MongoClient(uri);
-		async function run() {
-			try {
-				const database = dbClient.db('sb_servers');
-				const servers = database.collection('servers');
-				const query = { id: `${interaction.guild.id}` };
-				const server = await servers.findOne(query);
-				if (server != null) {
-					await interaction.reply(`This server is ${interaction.guild.id} (${server.id}) and has ${interaction.guild.memberCount} members\rGPTChecking = ${server.gptChecking}`);
-				} else {
-					await interaction.reply(`This server is  ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
-				};
-			} finally {
-				await dbClient.close();
-			}
-		}
-		run().catch(console.dir);
-	},
+  data: new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Provides information about the server."),
+  async execute(interaction) {
+    // launch MongoDB connection and query database
+    const { MongoClient } = require("mongodb");
+    const uri = process.env.MONGODB_URI;
+    const dbClient = new MongoClient(uri);
+    async function run() {
+      try {
+        const database = dbClient.db("sb_servers");
+        const servers = database.collection("servers");
+        const query = { id: `${interaction.guild.id}` };
+        const server = await servers.findOne(query);
+        if (server != null) {
+          await interaction.reply(
+            `This server is ${interaction.guild.id} (${server.id}) and has ${interaction.guild.memberCount} members\rGPTChecking = ${server.gptChecking}`
+          );
+        } else {
+          await interaction.reply(
+            `This server is  ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
+          );
+        }
+      } finally {
+        await dbClient.close();
+      }
+    }
+    run().catch(console.dir);
+  },
 };
--- commands/user.js
+++ commands/user.js
@@ -1,32 +1,36 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('Provides information about the user.'),
-	async execute(interaction) {
-		const { MongoClient } = require("mongodb");
-		const uri = process.env.MONGODB_URI;
-		const dbClient = new MongoClient(uri);
-		async function run() {
-			try {
-				const database = dbClient.db('sb_users');
-				const admins = database.collection('admins');
-				const query = { uid: `${interaction.user.id}` };
-				const user = await admins.findOne(query);
+  data: new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Provides information about the user."),
+  async execute(interaction) {
+    const { MongoClient } = require("mongodb");
+    const uri = process.env.MONGODB_URI;
+    const dbClient = new MongoClient(uri);
+    async function run() {
+      try {
+        const database = dbClient.db("sb_users");
+        const admins = database.collection("admins");
+        const query = { uid: `${interaction.user.id}` };
+        const user = await admins.findOne(query);
 
-				// Respond to the command with user information
-				// If user exists in the database, print database information, if not, print base discord information.
-				if (user != null) {
-					await interaction.reply(`Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rYou joined this server on ${interaction.member.joinedAt} \rand your role in my backend system is ***${user.role}***`);
-				} else {
-					await interaction.reply(`Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rand you joined this server on ${interaction.member.joinedAt}`);
-				};
-			} finally {
-				// Ensure the MongoDB client is closed
-				await dbClient.close();
-			}
-		}
-		run().catch(console.dir);
-	},
+        // Respond to the command with user information
+        // If user exists in the database, print database information, if not, print base discord information.
+        if (user != null) {
+          await interaction.reply(
+            `Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rYou joined this server on ${interaction.member.joinedAt} \rand your role in my backend system is ***${user.role}***`
+          );
+        } else {
+          await interaction.reply(
+            `Hi ${interaction.user.username}! \rYour UID is: ${interaction.user.id} \rand you joined this server on ${interaction.member.joinedAt}`
+          );
+        }
+      } finally {
+        // Ensure the MongoDB client is closed
+        await dbClient.close();
+      }
+    }
+    run().catch(console.dir);
+  },
 };
